cbb463481de547f460438afa03159a5ca6da5ebd
benlove <benuklove@gmail.com>
2018-03-10 20:09:48 -0600

    Add registration functionality

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     url(r'^password-reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^password-reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    # account register url
+    url(r'^register/$', views.register, name='register'),
     ]
